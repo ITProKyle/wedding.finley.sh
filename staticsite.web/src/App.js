e4acf689873c185448250c88f08b8ebb3479355f
Kyle Finley <kyle@finley.sh>
2020-08-03 18:59:11 -0700

    add initial infrastructure

---
+++ staticsite.web/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-header-date">
+          10 / 10 / 2020
+        </p>
+        <p className="App-header-description">
+          More info will be provided here closer to the date.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
