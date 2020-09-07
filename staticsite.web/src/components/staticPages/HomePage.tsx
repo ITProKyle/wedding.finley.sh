f07c56a66c3cc530b7bc88d54ababddc37bfc555
Kyle Finley <kyle@finley.sh>
2020-09-07 16:31:53 -0700

    add calendar buttons

--- staticsite.web/src/components/staticPages/HomePage.tsx
+++ staticsite.web/src/components/staticPages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Card, Container, Divider, Header } from 'semantic-ui-react';
+import { Button, Card, Container, Divider, Grid, Header, Icon } from 'semantic-ui-react';
 import 'src/components/staticPages/HomePage.scss';
 import { ThemeContext } from 'src/context/ThemeContext';
 import CountdownClock from 'src/components/countdown/CountdownClock';
@@ -31,7 +31,49 @@ const HomePage: React.FC = () : JSX.Element => {
       >
         More info will be provided here closer to the date
       </Header>
+
+      <Grid centered className='add-to-calendar' textAlign='center'>
+        <Grid.Row centered columns={2}>
+          <Grid.Column textAlign='center'>
+            <Header style={{ color: settings.fontColor }}>
+              Ceremony
+            </Header>
+            <Button
+              icon
+              inverted={theme === 'dark'}
+              labelPosition='left'
+              onClick={event => {
+                event.preventDefault();
+                window.open('http://evt.to/immsmhdw')
+              }}
+            >
+              <Icon name='calendar alternate outline'/>
+              Add to Calendar
+            </Button>
+          </Grid.Column>
+          <Grid.Column textAlign='center'>
+            <Header style={{ color: settings.fontColor }}>
+              Reception
+            </Header>
+            <Button
+              disabled
+              icon
+              inverted={theme === 'dark'}
+              labelPosition='left'
+              onClick={event => {
+                event.preventDefault();
+                window.open('#')
+              }}
+            >
+              <Icon name='calendar alternate outline'/>
+              Add to Calendar
+            </Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
       <Divider inverted={theme === 'dark'} />
+
       <Header
         inverted={theme === 'dark'}
         size='large'
@@ -43,6 +85,7 @@ const HomePage: React.FC = () : JSX.Element => {
         <PayPalMe />
         <AmazonWeddingRegistry />
       </Card.Group>
+
     </Container>
   )
 }
